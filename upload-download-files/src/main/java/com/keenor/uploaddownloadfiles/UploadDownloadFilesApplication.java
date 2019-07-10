4eba8f2e1da870944914c1b85a5159800bfef051
chenliuchun <chenliuchun@zccp.com>
2019-07-10 18:00:17 +0800

    init add upload-download

---
+++ upload-download-files/src/main/java/com/keenor/uploaddownloadfiles/UploadDownloadFilesApplication.java
@@ -0,0 +1,28 @@
+package com.keenor.uploaddownloadfiles;
+
+import com.keenor.uploaddownloadfiles.storage.StorageProperties;
+import com.keenor.uploaddownloadfiles.storage.StorageService;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
+public class UploadDownloadFilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UploadDownloadFilesApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
+    }
+
+}
